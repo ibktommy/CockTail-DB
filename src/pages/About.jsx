5f35695bf181ab62f97da9ccffa473f0da5db96a
ibktommy <atomquartz@gmail.com>
2022-08-16 12:46:33 +0100

    Add elements to the About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
-  )
+		<section className="section about-section">
+			<h1 className="section-title">About Us</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+				mollitia natus repudiandae modi nostrum temporibus dolorem praesentium
+				dicta magni soluta ea voluptatem deserunt, velit similique voluptatibus
+				veritatis rerum deleniti, voluptates perferendis, expedita tenetur?
+				Nobis natus officia, dolorum adipisci aspernatur commodi!
+			</p>
+		</section>
+	);
 }
 
 export default About
