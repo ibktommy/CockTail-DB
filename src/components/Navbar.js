edbd117433a81de7f89584089d79aa1f566648fd
ibktommy <atomquartz@gmail.com>
2022-08-16 12:43:54 +0100

    Work on the Navbar component and use 'Link' from react-router-dom' to include links to both the Home And ABout page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import logo from '../assests/logo.svg'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='navbar'>
+      <div className="nav-center">
+        <Link to='/'>
+          <img src={logo} alt='Logo' />
+        </Link>
+
+        <ul className='nav-links'>
+          <li>
+            <Link to='/'>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to='/about'>
+              About
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
