b48d5e3a4bed38020e6897ae07fd06a847bf587a
ibktommy <atomquartz@gmail.com>
2022-08-16 12:34:00 +0100

    Import Pages and using react-router-dom methods to route to the secified pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route}  from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About'
+import Error from './pages/Error'
+import SingleCockTail from './pages/SingleCockTail'
+
 function App() {
   return (
-    <h1>Cocktail DB</h1>
+    <Router>
+      <Navbar />
+      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/error' element={<Error />} />
+        <Route path='/cocktail/:id' element={<SingleCockTail />} />
+      </Routes>
+    </Router>
   );
 }
 
