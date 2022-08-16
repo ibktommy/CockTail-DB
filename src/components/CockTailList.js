b7f4f0bbd1032aaa861fa1931b015f7558d4546d
ibktommy <atomquartz@gmail.com>
2022-08-16 15:37:22 +0100

    Add states from the contextProvider to some components where the states are needed

--- src/components/CockTailList.js
+++ src/components/CockTailList.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useGlobalContext } from '../context'
+import Loading from './Loading'
 
 const CockTailList = () => {
+  // Getting cockTailData state from contextProvider
+  const { CockTailData, isLoading } = useGlobalContext()
+
+  if (isLoading) {
+    return <Loading />
+  }
+
   return (
     <div>CockTailList</div>
   )
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
+  // Getting searchTerm-state from the contextProvider
+  const { setSearchTerm } = useGlobalContext()
+  
   return (
     <div>SearchForm</div>
   )
