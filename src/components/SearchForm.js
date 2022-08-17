b2b632b60e1fb09a88fc4f3474bcaca2ac041805
ibktommy <atomquartz@gmail.com>
2022-08-17 17:20:55 +0100

    Create function to set the searchTerm-state to update as we input values in the form-input field

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
   // Getting searchTerm-state from the contextProvider
   const { setSearchTerm } = useGlobalContext()
+  // Seeting useRef Hook for the Form Input-Field
+  const { searchValue } = useRef('')
+
+  // function to monitor input value
+  const searchCockTailHandler = () => {
+    setSearchTerm(searchValue)
+  }
   
   return (
-    <div>SearchForm</div>
+    <section className="section search">
+      <form className="search-form">
+        <div className="form-control">
+          <label htmlFor="">Search Your Favorite Cocktail</label>
+          <input type="text" id="name" ref={searchValue} onChange={searchCockTailHandler}/>
+        </div>
+      </form>
+    </section>
   )
 }
 
