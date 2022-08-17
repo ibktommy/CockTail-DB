d64448371a385a02c6dac4a2423686200f93df47
ibktommy <atomquartz@gmail.com>
2022-08-17 22:09:15 +0100

    Structure the SingleCockTail elements and how they are displayed

--- src/pages/SingleCockTail.jsx
+++ src/pages/SingleCockTail.jsx
@@ -86,7 +86,55 @@ const SingleCockTail = () => {
 	const { name, image, info, category, glass, instructions, ingredients } =
 		cockTailDetail;
 	
-	return <div>{id}</div>;
+	return (
+		<section className="section cocktail-section">
+			<Link to='/' className="btn btn-primary">Back Home</Link>
+			<h2 className="section-title">{name}</h2>
+			<div className="drink">
+				<img src={image} alt={name} />
+				<div className="drink-info">
+					<p>
+						<span className="drink-data">
+							name : 
+						</span>
+						{name}
+					</p>
+					<p>
+						<span className="drink-data">
+							category : 
+						</span>
+						{category}
+					</p>
+					<p>
+						<span className="drink-data">
+							info : 
+						</span>
+						{info}
+					</p>
+					<p>
+						<span className="drink-data">
+							glass : 
+						</span>
+						{glass}
+					</p>
+					<p>
+						<span className="drink-data">
+							instructions : 
+						</span>
+						{instructions}
+					</p>
+					<p>
+						<span className="drink-data">
+							ingredients : 
+						</span>
+						{ingredients.map((item, index) => {
+							return item ? <span key={index}>{item}</span> : null
+						})}
+					</p>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default SingleCockTail;
